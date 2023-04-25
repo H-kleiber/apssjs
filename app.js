ab579c43443636745ad00468f84502be9dfc06e0
H-kleiber <limnuev25@gmail.com>
2023-04-24 21:52:46 -0500

    js

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+let name="herol kleiber";
+let lastname="huamani noñonca";
+let country="Peru"
+
+let age=26;
+let phonenumber=992911553;
+let homephone=21321312321;
+
+let obj1={
+    name : "key",
+    amount: 1
+}
+let obj2={
+    name : "phone",
+    amount: 5
+}
+let obj3={
+    name: "headphones",
+    amount: 10
+}
+
+let array_user=[["fabio"      ,  12344   ],
+                ["alejhandro" ,  12321312],
+                ["francisco"  ,  21321   ]];
+let idat;
+let x=null;
+
+console.log("Nombre:",name,lastname," - Ciudad :",country);
+console.log("edad:", age, "numero celular:",phonenumber, "telefono fijo:", homephone);
+console.log(typeof(obj1));
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
+console.log(array_user);
